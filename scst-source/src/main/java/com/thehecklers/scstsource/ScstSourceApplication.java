bdcd31c6bf3fba194904ba6c4d03734ea70e2217
Mark Heckler <mark.heckler@gmail.com>
2019-03-15 23:11:24 +0000

    Minor cleanup

--- scst-source/src/main/java/com/thehecklers/scstsource/ScstSourceApplication.java
+++ scst-source/src/main/java/com/thehecklers/scstsource/ScstSourceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
